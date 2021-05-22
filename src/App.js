bd0a2bfe8c6f5e4a7648317dc7da559e19a517a7
pedromlopesnunes <pedromlopesnunes@gmail.com>
2021-05-22 18:11:13 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+
+import oeqtam from'./oeqtam.jpg';
+import pro from'./prodigo.png';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  const handleClick = async() => {
+    fetch("http://localhost:3001/")
+    .then((res) => res.json())
+    .then((data) => {
+      setData(data.message)
+    }
+    );
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pro} className="App-logo" alt="logo" />
+        <p>{!data ? "Tens comichão no olho do cu? Chama-me Capitão Gancho" : data}</p>
+        <button onClick={handleClick}>Pró-Di-Go</button>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
