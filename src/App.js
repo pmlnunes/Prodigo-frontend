44297a727e47f8672d4c52f118a1cce4fac7321c
pedromlopesnunes <pedromlopesnunes@gmail.com>
2021-05-22 18:35:07 +0100

    Fixed server connection problem

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   const handleClick = async() => {
-    fetch("http://localhost:3001/")
+    fetch("https://pro-di-go.herokuapp.com/")
     .then((res) => res.json())
     .then((data) => {
       setData(data.message)
